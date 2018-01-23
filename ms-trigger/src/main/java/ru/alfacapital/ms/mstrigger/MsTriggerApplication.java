8f70b4c589537b99647f0c04a16e2836cebf16c8
Konstantin Kharitonov <kkharitonov@luxoft.com>
2018-01-23 12:45:19 +0300

    initial commit

---
+++ ms-trigger/src/main/java/ru/alfacapital/ms/mstrigger/MsTriggerApplication.java
@@ -0,0 +1,12 @@
+package ru.alfacapital.ms.mstrigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTriggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTriggerApplication.class, args);
+	}
+}
---
+++ ms-trigger/src/test/java/ru/alfacapital/ms/mstrigger/MsTriggerApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.alfacapital.ms.mstrigger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsTriggerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
