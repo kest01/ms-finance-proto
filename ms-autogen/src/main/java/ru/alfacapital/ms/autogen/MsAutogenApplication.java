43995a8ba267a8cd0aef256393ddefed60d615d0
Konstantin Kharitonov <kkharitonov@luxoft.com>
2018-01-23 19:17:32 +0300

    Autogen service stub

---
+++ ms-autogen/src/main/java/ru/alfacapital/ms/autogen/MsAutogenApplication.java
@@ -0,0 +1,12 @@
+package ru.alfacapital.ms.autogen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAutogenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAutogenApplication.class, args);
+	}
+}
