49d639250e8936cab8d3bd4baac3006b7946ab15
Konstantin Kharitonov <kkharitonov@luxoft.com>
2018-01-25 16:28:01 +0300

    ETrader microservice stub

---
+++ ms-etrader/src/main/java/ru/alfacapital/ms/etrader/MsETraderApplication.java
@@ -0,0 +1,12 @@
+package ru.alfacapital.ms.etrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsETraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsETraderApplication.class, args);
+	}
+}
